a6fbe3f907c4f4c7b140450de79a42fc4f078340
Muyoung_Lee <10myl200@users.noreply.github.com>
2024-02-03 10:19:09 -0600

    Delete scripts/attach_tags_to_name.py

--- scripts/attach_tags_to_name.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-with open(sys.argv[1]) as INPUT:
-	for line in INPUT:
-		if line.startswith(">"):
-			print(line.strip().replace("\t", "_"))
-		else:
-			print(line.strip())
