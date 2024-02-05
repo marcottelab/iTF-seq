e385e923303fd56536841497de5618a49ef8c26f
Muyoung_Lee <10myl200@users.noreply.github.com>
2024-02-05 06:58:25 -0600

    Update print_QCed_cell_list.R

--- scripts/print_QCed_cell_list.R
+++ scripts/print_QCed_cell_list.R
@@ -1,7 +1,7 @@
 library(Seurat)
 
 # day 1
-data_matrix <- Read10X(data.dir="outs/filtered_feature_bc_matrix")
+data_matrix <- Read10X(data.dir="80TFs1d/outs/filtered_feature_bc_matrix")
 seurat_obj <- CreateSeuratObject(counts=data_matrix, min.cells=0, min.features=0, project="iTF_seq")
 rm(data_matrix)
 seurat_obj[["percent.mt"]] <- PercentageFeatureSet(seurat_obj, pattern="^mt-")
@@ -9,7 +9,7 @@ seurat_obj <- subset(seurat_obj, subset = nFeature_RNA > 1000 & percent.mt < 10 
 write.table(seurat_obj@meta.data, "d1.g1k_mt10_umi10k.tsv", sep="\t", quote=F)
 
 # day 3
-data_matrix <- Read10X(data.dir="outs/filtered_feature_bc_matrix")
+data_matrix <- Read10X(data.dir="80TFs3d/outs/filtered_feature_bc_matrix")
 seurat_obj <- CreateSeuratObject(counts=data_matrix, min.cells=0, min.features=0, project="iTF_seq")
 rm(data_matrix)
 seurat_obj[["percent.mt"]] <- PercentageFeatureSet(seurat_obj, pattern="^mt-")
@@ -17,7 +17,7 @@ seurat_obj <- subset(seurat_obj, subset = nFeature_RNA > 1000 & percent.mt < 10 
 write.table(seurat_obj@meta.data, "d3.g1k_mt10_umi10k.tsv", sep="\t", quote=F)
 
 # day 5
-data_matrix <- Read10X(data.dir="outs/filtered_feature_bc_matrix")
+data_matrix <- Read10X(data.dir="80TFs5d/outs/filtered_feature_bc_matrix")
 seurat_obj <- CreateSeuratObject(counts=data_matrix, min.cells=0, min.features=0, project="iTF_seq")
 rm(data_matrix)
 seurat_obj[["percent.mt"]] <- PercentageFeatureSet(seurat_obj, pattern="^mt-")
